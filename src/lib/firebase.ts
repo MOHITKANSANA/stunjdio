2c3a40cdff788c300428ead13eab635bc9c24cad
Mohit Kansana <mohitkansana82@gmail.com>
2025-10-14 10:50:27 +0000

    Try fixing this error: `Console Error: [2025-10-14T10:43:03.224Z] @fireb

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCowK1pgv_fGzCo65c-7_-9vTIkrO628yM",
-  authDomain: "go-swami-coching-classes.firebaseapp.com",
-  projectId: "go-swami-coching-classes",
-  storageBucket: "go-swami-coching-classes.firebasestorage.app",
-  messagingSenderId: "1064325037320",
-  appId: "1:1064325037320:web:d9ce26cc409e20702700bd"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "YOUR_API_KEY",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "YOUR_AUTH_DOMAIN",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "YOUR_PROJECT_ID",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "YOUR_STORAGE_BUCKET",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "YOUR_MESSAGING_SENDER_ID",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "YOUR_APP_ID"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
